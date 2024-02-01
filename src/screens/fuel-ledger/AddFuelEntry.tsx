ce8a476c969056268d6d68cb2b19d2e5c0c5ee4f
Jogy Felix <jogyfelix1@gmail.com>
2024-02-02 00:35:08 +0530

    fule ledger changes

--- src/screens/fuel-ledger/AddFuelEntry.tsx
+++ src/screens/fuel-ledger/AddFuelEntry.tsx
@@ -1,12 +1,43 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import ParentView from '../../components/general/ParentView';
+import { View, Text, StatusBar } from 'react-native';
+import { theme } from '../../constants/theme';
+import InputData from '../../components/general/InputData';
+import PrimaryBtn from '../../components/general/PrimaryBtn';
 
 const AddFuelEntry = () => {
   return (
-    <ParentView>
-      <Text>Hello</Text>
-    </ParentView>
+    <View style={{ flex: 1, backgroundColor: theme.COLORS.black }}>
+      <StatusBar backgroundColor={theme.COLORS.cardBg} />
+      <View
+        style={[
+          {
+            backgroundColor: theme.COLORS.cardBg,
+            height: 200,
+          },
+        ]}
+      >
+        <Text
+          style={[
+            {
+              color: theme.COLORS.text,
+              fontSize: 24,
+              marginHorizontal: 16,
+              textAlign: 'center',
+              position: 'absolute',
+              bottom: 16,
+              fontFamily: theme.FONTS.default,
+            },
+          ]}
+        >
+          Add fuel entry
+        </Text>
+      </View>
+      <InputData placeholder="Cost" />
+      <InputData placeholder="Total amount" />
+      <InputData placeholder="Note" />
+      <InputData placeholder="Date" />
+      <PrimaryBtn title="Add entry" />
+    </View>
   );
 };
 
--- src/screens/fuel-ledger/FuelLedger.tsx
+++ src/screens/fuel-ledger/FuelLedger.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, SectionList, StyleSheet, StatusBar } from 'react-native';
+import { View, Text, SectionList, StyleSheet, StatusBar, useWindowDimensions } from 'react-native';
 import ParentView from '../../components/general/ParentView';
 import { NavigationProp, ParamListBase } from '@react-navigation/native';
 import screenNames from '../../constants/screenNames';
@@ -7,10 +7,12 @@ import Fab from '../../components/general/Fab';
 import Animated, {
   Extrapolate,
   interpolate,
+  interpolateColor,
   useAnimatedStyle,
   useSharedValue,
   withTiming,
 } from 'react-native-reanimated';
+import { theme } from '../../constants/theme';
 
 interface Props {
   navigation: NavigationProp<ParamListBase>;
@@ -35,9 +37,9 @@ const DATA = [
   },
 ];
 
-const Max_Header_Height = 200;
-const Min_Header_Height = 70;
-const Scroll_Distance = Max_Header_Height - Min_Header_Height;
+const MAX_HEADER_HEIGHT = 200;
+const MIN_HEADER_HEIGHT = 70;
+const SCROLL_DISTANCE = MAX_HEADER_HEIGHT - MIN_HEADER_HEIGHT;
 
 const FuelLedger = ({ navigation }: Props) => {
   const scrollOffsetY = useSharedValue(0);
@@ -46,31 +48,57 @@ const FuelLedger = ({ navigation }: Props) => {
     return {
       height: interpolate(
         scrollOffsetY.value,
-        [0, Scroll_Distance],
-        [Max_Header_Height, Min_Header_Height],
+        [0, SCROLL_DISTANCE],
+        [MAX_HEADER_HEIGHT, MIN_HEADER_HEIGHT],
         Extrapolate.CLAMP
+      ),
+      backgroundColor: interpolateColor(
+        scrollOffsetY.value,
+        [0, SCROLL_DISTANCE],
+        ['transparent', theme.COLORS.cardBg]
       ),
     };
   });
+
+  const textStyle = useAnimatedStyle(() => {
+    return {
+      fontSize: interpolate(scrollOffsetY.value, [0, SCROLL_DISTANCE], [28, 24], Extrapolate.CLAMP),
+    };
+  });
+
   return (
-    <ParentView type="center">
+    <View style={styles.container}>
       <Animated.View
         style={[
           {
-            // position: 'absolute',
-            // top: 0,
-            // left: 0,
-            // right: 0,
-
-            backgroundColor: 'red',
+            backgroundColor: theme.COLORS.black,
           },
           rStyle,
         ]}
-      />
+      >
+        <Animated.Text
+          style={[
+            {
+              color: theme.COLORS.text,
+              fontSize: 24,
+              marginHorizontal: 16,
+              textAlign: 'center',
+              position: 'absolute',
+              bottom: 16,
+              fontFamily: theme.FONTS.default,
+            },
+            textStyle,
+          ]}
+        >
+          Fuel Ledger
+        </Animated.Text>
+      </Animated.View>
 
       <SectionList
         sections={DATA}
         keyExtractor={(item, index) => item + index}
+        // style={{ marginTop: StatusBar.currentHeight }}
+        contentContainerStyle={{ marginHorizontal: 16 }}
         scrollEventThrottle={16}
         onScroll={(event) => {
           scrollOffsetY.value = withTiming(event.nativeEvent.contentOffset.y, { duration: 100 });
@@ -84,15 +112,14 @@ const FuelLedger = ({ navigation }: Props) => {
       />
 
       <Fab onPress={() => navigation.navigate(screenNames.FUEL_ENTRY_SCREEN)} />
-    </ParentView>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: StatusBar.currentHeight,
-    marginHorizontal: 16,
+    backgroundColor: theme.COLORS.black,
   },
   item: {
     backgroundColor: '#f9c2ff',
