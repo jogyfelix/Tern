ae7af24506ed94357c15a5dc177cb8a18ca84c21
Jogy Felix <jogyfelix1@gmail.com>
2024-02-05 19:14:45 +0530

    style: profile screen basic design added

--- src/screens/profile/Profile.tsx
+++ src/screens/profile/Profile.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StatusBar, TouchableOpacity } from 'react-native';
+import { theme } from '../../constants/theme';
+import { Icon, VStack } from '@gluestack-ui/themed';
+import { User2 } from 'lucide-react-native';
+import SecondaryBtn from '../../components/SecondaryBtn';
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Hello</Text>
-    </View>
+    <VStack
+      flex={1}
+      backgroundColor={theme.COLORS.black}
+      paddingHorizontal={theme.DIMENSIONS.defaultHorizontalMargin}
+    >
+      <StatusBar backgroundColor={theme.COLORS.black} />
+      <VStack alignItems="center" marginVertical={88} alignSelf="center">
+        <View style={{ backgroundColor: theme.COLORS.cardBg, padding: 10, borderRadius: 20 }}>
+          <Icon as={User2} style={{ padding: 16 }} color={theme.COLORS.text} />
+        </View>
+
+        <Text style={{ fontFamily: theme.FONTS.default, color: theme.COLORS.text, fontSize: 24 }}>
+          Jogy Felix
+        </Text>
+      </VStack>
+      <SecondaryBtn title="Sync data" />
+    </VStack>
   );
 };
 
